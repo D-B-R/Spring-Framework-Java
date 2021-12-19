0d4c7e710260153cf5c7a688b5285564f20effcf
D-B-R <daniel.b.rocha@hotmail.com>
2021-12-19 10:44:49 -0300

    2.0

--- gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/novaEmpresa")
 public class NovaEmpresaServlet extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response)
@@ -19,6 +20,8 @@ public class NovaEmpresaServlet extends HttpServlet {
 		System.out.println("cadastrando nova empresa ");
 		String nomeEmpresa = request.getParameter("nome");			
 		PrintWriter out = response.getWriter();
+		
+		
 		out.println("<html><body> Empresa " + nomeEmpresa + " cadastrada com sucesso! </body></html>");
 	}
 	
