63fb5507ae569d6a78a77df40b9d16b8c210d93a
D-B-R <daniel.b.rocha@hotmail.com>
2021-12-19 10:40:34 -0300

    2.0

--- gerenciador/src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -24,8 +24,7 @@ public class OiMundoServlet extends HttpServlet {
 		
 		
 		out.print("<html>");		
-		out.print("<body>");
-		
+		out.print("<body>");		
 		out.print("oi mundo parabens");
 		
 		out.print("<body>");
